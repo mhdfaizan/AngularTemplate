43cfec31a92726dd9cb21f888f62aaccd40ddfbc
mhdfaizan <agile.faizan@gmail.com>
2018-12-11 13:07:47 +0500

    committing complete project

---
+++ ngApp/src/app/special-events/special-events.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { EventService } from '../event.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-special-events',
+  templateUrl: './special-events.component.html',
+  styleUrls: ['./special-events.component.css']
+})
+export class SpecialEventsComponent implements OnInit {
+
+  specialEvents = [];
+  constructor(private _eventService: EventService, private _router: Router) { }
+
+  ngOnInit() {
+    this._eventService.getSpecialEvents()
+      .subscribe(
+        res => this.specialEvents = res,
+        error => {
+          if (error instanceof HttpErrorResponse){
+            if(error.status === 401){
+              this._router.navigate(['/login']);
+            }
+          }
+        }
+      );
+  }
+}
